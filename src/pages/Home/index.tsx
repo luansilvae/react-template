c210a9c6df28d2dc37e95df2a661e117e58348b7
Luan Silva <luan.owsla@gmail.com>
2022-03-21 20:59:09 -0300

    add eslint + prettier

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 import style from './Home.module.scss'
 
 export const Home: React.FC = () => {
   return (
-    <div className={style.Container} >
+    <div className={style.Container}>
       <h1>React template</h1>
     </div>
-  );
+  )
 }
